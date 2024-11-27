9dd13114aac67d09a6527285fe2e86d0184ce679
Binxx3600 <150201505+Binxx3600@users.noreply.github.com>
2024-11-27 18:34:32 -0500

    particle test

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
     }
 
-    const particleCount = 150; // Increase the number of particles
+    const particleCount = 3000; // Increase the number of particles
     const particles = [];
 
     // Generate particles
